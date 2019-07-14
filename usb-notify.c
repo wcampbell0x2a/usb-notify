f295616102750bb4a4f1b399f44f487b0861a4f2
userWayneCampbell <wcampbell1995@gmail.com>
2019-07-14 16:54:39 -0400

    Initial commit
    Signed-off-by: userWayneCampbell <wcampbell1995@gmail.com>

---
+++ usb-notify.c
@@ -0,0 +1,6 @@
+#include "usb-notify.h"
+
+int main()
+{
+  printf("hello world\n");
+}
---
+++ usb-notify.h
@@ -0,0 +1,5 @@
+#ifndef USB_NOTIFY_H
+#define USB_NOTIFY_H
+#include <libudev.h>
+#include <libnotify/notify.h>
+#endif
