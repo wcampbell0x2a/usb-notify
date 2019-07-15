cad6b87d7bfd8c65426356bb0729b0dc962316d3
userWayneCampbell <wcampbell1995@gmail.com>
2019-07-14 20:00:40 -0400

    Add display notification for bind action
    Checks for the bind action from the udev device and then checks
    if it's the bind action (right before the add action). Then displays the
    vid pid serial of device to terminal and libnotify
    
    Signed-off-by: userWayneCampbell <wcampbell1995@gmail.com>

--- usb-notify.c
+++ usb-notify.c
@@ -5,25 +5,58 @@ void sig_handler()
   printf("[#] received signal, exiting...\n");
 }
 
-void display_notification(struct udev_device* dev)
+/*
+ * Display Notification
+ *
+ * Display notification of the device informatino if the device is of the
+ * 'bind' udev action.
+ *
+ * @param dev, udev_device from udev
+ * @return int of success
+ */
+int display_notification(struct udev_device* dev)
 {
   if (dev)
   {
     if (udev_device_get_devnode(dev))
     {
-      NotifyNotification * n_usb = notify_notification_new("usb-notify", "Yikes", "dialog-information");
-      notify_notification_show(n_usb, NULL);
-      g_object_unref(G_OBJECT(n_usb));
+
+      /* Check if action is bind */
+      const char* action = udev_device_get_action(dev);
+      if (!strcmp(action, "bind"))
+      {
+        const char *vendor  = udev_device_get_sysattr_value(dev, "idVendor");
+        const char *product = udev_device_get_sysattr_value(dev, "idProduct");
+        const char *serial  = udev_device_get_sysattr_value(dev, "serial");
+
+        char *message;
+        if (0 > asprintf(&message,
+	        "Serial #: %s\n"
+             "vid-pid: %s-%s\n"
+             "Action: %s\n",
+	        serial, vendor, product, action))
+        {
+            printf("[!] Error allocating char\n");
+        }
+        printf("[#] Displaying message: \n%s\n", message);
+        NotifyNotification * n_usb = notify_notification_new("usb-notify", message, "dialog-information");
+        notify_notification_show(n_usb, NULL);
+        g_object_unref(G_OBJECT(n_usb));
+      }
+      else
+      {
+        return 1;
+      }
     }
   }
   udev_device_unref(dev);
+  return 0;
 }
 
 void monitor_devices(struct udev* udev)
 {
 
   struct udev_monitor* mon = udev_monitor_new_from_netlink(udev, "udev");
-  printf("1\n");
 
   udev_monitor_filter_add_match_subsystem_devtype(mon, "usb", NULL);
   udev_monitor_enable_receiving(mon);
